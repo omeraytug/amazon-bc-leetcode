948cfcdeff08281b53315809bc50652198d34ee9
Omer Aytug <aytugomer00@gmail.com>
2025-09-22 22:59:03 +0200

    first week

--- leetcode-solutions.py
+++ leetcode-solutions.py
@@ -1,9 +1,21 @@
-#3110
-word = "hello"
-score = 0
-for i in range(len(word) - 1):
-	score += abs(ord(word[i]) - ord(word[i + 1]))
-print(score)
+# #3110
+# word = "hello"
+# score = 0
+# for i in range(len(word) - 1):
+# 	score += abs(ord(word[i]) - ord(word[i + 1]))
+# print(score)
 
 
 
+# 1119
+def remove_vowels(s):
+    res: ""
+    vowels "aeiou"
+    for i in s:
+        if i not in vowels:
+            result += i
+
+    return result
+
+#def remove.vowels(s): 
+#   return "".join(a for a in s if a not in"aeiou")
