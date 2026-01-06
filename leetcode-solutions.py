8f1b202559cabea94ee020dde577275da12bb453
Omer Aytug <aytugomer00@gmail.com>
2026-01-06 18:45:55 +0100

    solution

--- leetcode-solutions.py
+++ leetcode-solutions.py
@@ -258,3 +258,23 @@ class Solution:
     def judgeCircle(self, moves: str) -> bool:
         return moves.count('U') == moves.count('D') and \
                moves.count('L') == moves.count('R')
+
+
+
+# 824 Goat Latin
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        vowels = set("aeiouAEIOU")
+        words = sentence.split()
+        result = []
+
+        for i, word in enumerate(words, start=1):
+            if word[0] in vowels:
+                new_word = word + "ma"
+            else:
+                new_word = word[1:] + word[0] + "ma"
+
+            new_word += "a" * i
+            result.append(new_word)
+
+        return " ".join(result)
